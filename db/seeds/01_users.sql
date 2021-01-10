29960ab7047c0b35f3df5c643889fde0cb963664
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-10 02:38:00 +0000

    Add Fake users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,2 +1,15 @@
--- Users table seeds here (Example)
 INSERT INTO users (name, email, password, first_name, last_name, city, gender) VALUES ('Alicindra', 'alice@gmail.com', 'password', 'Alice', 'Cooper', 'Fake City', 'female' );
+INSERT INTO users (name, email, password, first_name, last_name, city, gender) VALUES ('Armhil', 'lera_hahn@dickens.org', 'password', 'Armand', 'Hilll', 'Vancouver', 'male' );
+INSERT INTO users (name, email, password) VALUES ('Brian', 'percy_ratke@cornelius.biz', 'password');
+INSERT INTO users (name, email, password, first_name, last_name, city, gender) VALUES ('Stef', 'darius.homenick@tod.ca', 'password', 'Stephanie', 'Wolff', 'Calgary', 'female' );
+INSERT INTO users (name, email, password, first_name, last_name, city, gender) VALUES ('Dison', 'derrick_pollich@gmail.com', 'password', 'Elliot', 'Dickinson', 'North Pole', 'female' );
+INSERT INTO users (name, email, password) VALUES ('Veum', 'elisha_wisoky@gmail.com', 'password');
+INSERT INTO users (name, email, password, first_name, last_name, city) VALUES ('Ebba', 'ebba.deckow@yahoo.com', 'password', 'Lloyd', 'Boehm', 'Toronto');
+INSERT INTO users (name, email, password, first_name, last_name, city, gender) VALUES ('Casss', 'miguel.barrows@yahoo.com', 'password', 'Erna', 'Cassin', 'Real City', 'female' );
+INSERT INTO users (name, email, password) VALUES ('OWell', 'gaetano_cummerata@hotmail.com', 'password');
+INSERT INTO users (name, email, password, first_name, last_name, city, gender) VALUES ('Bredis', 'alysha.daniel@boyer.tv', 'password', 'Edison', 'Brown', 'Abbotsford', 'female' );
+INSERT INTO users (name, email, password, first_name, last_name, city, gender) VALUES ('Morlay', 'bradtke.mallie@yahoo.com', 'password', 'Lionel', 'Morar', 'Burnaby', 'female' );
+INSERT INTO users (name, email, password, first_name, last_name, city, gender) VALUES ('Katti', 'kattie_dibbert@winnifred.name', 'password', 'Donnie', 'Lueilwitz', 'Chilliwack', 'female' );
+INSERT INTO users (name, email, password, first_name, last_name, city) VALUES ('OWell', 'elisha_wisoky@gmail.com', 'password', 'Obie', 'Howell', 'Coquitlam');
+INSERT INTO users (name, email, password) VALUES ('Hiram', 'kristopher.shanahan@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Lynn', 'wisoky@gmail.com', 'password');
