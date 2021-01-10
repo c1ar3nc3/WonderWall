a0a9c0c70377bb537b9fb17a1e2ab0d018e717a2
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-10 01:40:25 +0000

    Create a users table and database

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,2 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, first_name, last_name, city, gender) VALUES ('Alicindra', 'alice@gmail.com', 'password', 'Alice', 'Cooper', 'Fake City', 'female' );
